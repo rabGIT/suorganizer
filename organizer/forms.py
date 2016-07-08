809c40a12cfcffb6bef6d6bc23b3ff9a2bcaad04
rabGIT <rabGIT@users.noreply.github.com>
2016-07-08 15:17:29 -0700

    Chapter 7 complete: added forms

---
+++ organizer/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Tag, NewsLink, Startup
+
+
+class SlugCleanMixin:
+    # Mixin class for slug cleaning method.
+
+    def clean_slug(self):
+        new_slug = (self.cleaned_data['slug'].lower())
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "create".')
+        return new_slug
+
+
+class TagForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+    def clean_name(self):
+        return self.cleaned_data['name'].lower()
+
+
+class NewsLinkForm(forms.ModelForm):
+    class Meta:
+        model = NewsLink
+        fields = '__all__'
+
+
+class StartupForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = '__all__'
